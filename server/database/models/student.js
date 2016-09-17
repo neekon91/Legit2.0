7610c85cc888cb5222fa492d186266ba9a38d281
Neekon <neekon91@gmail.com>
2016-09-16 19:37:16 -0700

    test closes #17

--- server/database/models/student.js
+++ server/database/models/student.js
@@ -1,9 +1,9 @@
-var Sequelize = require('sequelize');
-var db = require('../database_config.js');
-
-var Student = db.define('Student', {
-  first: Sequelize.STRING,
-  last: Sequelize.STRING
-});
-
-module.exports = Student;
+// var Sequelize = require('sequelize');
+// var db = require('../database_config.js');
+//
+// var Student = db.define('Student', {
+//   first: Sequelize.STRING,
+//   last: Sequelize.STRING
+// });
+//
+// module.exports = Student;
