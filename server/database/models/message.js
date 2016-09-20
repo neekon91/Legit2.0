6a562bfc3f43136b4f53cf3949a7e117aa92e318
strongharris <harris.lee121089@gmail.com>
2016-09-19 17:01:18 -0700

    fix closes #38

--- server/database/models/message.js
+++ server/database/models/message.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const message = new Schema({
-  receive_id: String,
-  sent_id: String,
+  teacher_id: String,
+  student_id: String,
   message: String,
   date: {type: Date, default: Date.now}
 });
+
 const Message = mongoose.model("Message", message);
 
 module.exports = Message;
+//
